91241c3c37d4cfb9202ce986a983eff54b9bcd52
Drinkaholik <marlinsquires123@gmail.com>
2026-09-04 15:21:30 +0200

    falling sand now works

--- game.cpp
+++ game.cpp
@@ -1,45 +1,149 @@
 #include "precomp.h"
 #include "game.h"
 #include <iostream>
+#include <vector>
+
+
+int PosToIndex(int x, int y)
+{
+	int r = x + y * SCRWIDTH;
+	
+	if (r <= SCRWIDTH * SCRHEIGHT)
+	{
+		return r;
+	}
+
+	else return SCRWIDTH * SCRHEIGHT;
+}
+
+
+struct Particle
+{
+	float2 pos;
+	bool blocked = false;
+	float2 moveDir = float2(0.0f, 1.0f);
+
+	int colour = 0xFF0000;
+
+	enum Type
+	{
+		Red, Green, Blue
+	} type;
+
+
+	void Move()
+	{
+
+		switch (type)
+		{
+			case Red:
+				if (!blocked)
+				{
+					pos += moveDir;
+				}
+				break;
+
+			case Green:
+				if (!blocked)
+				{
+					pos += moveDir;
+				}
+				else
+				{
+					moveDir = (0, -1);
+				}
+				break;
+
+			case Blue:
+				if (!blocked)
+				{
+					pos += moveDir;
+				}
+				else
+				{
+					moveDir = (-1, 0);
+				}
+				break;
+		}
+		
+	}
+
+	void CheckBlocked(Surface* scr)
+	{
+		if (blocked) return;
+		
+		bool h = pos.y + 1 == SCRHEIGHT;
+		bool b = scr->pixels[PosToIndex(pos.x, pos.y + 1)] != 0x000000;
+		if (h || b)
+		{
+			blocked = true;
+		}
+
+
+	}
+
+	void Draw(Surface* scr)
+	{
+		scr->Plot(pos.x, pos.y, colour);
+	}
 
 
 
+	Particle(float2 spawnPos) : pos(spawnPos)
+	{
+		type = static_cast<Type>(rand() % 3);
+
+		switch (type)
+		{
+			case Red:
+				colour = 0xFF0000;
+				break;
+			case Green:
+				colour = 0x00FF00;
+				break;
+			case Blue:
+				colour = 0x0000FF;
+				break;
+		}
+	};
+
+};
+
+
+
+vector<Particle> particles;
 
 
 
 void Game::Init()
 {
-	// Bitwise manipulation practice
-	/*Surface s("assets/nc2tiles.png");
-
-	uint* ptr = s.pixels;
-
-	int length = s.width * s.height;
-
-
-	for (int i = 0; i < length; i++)
-	{
-		ptr[i] &= 0xFF0000;
-	}
-
-	s.CopyTo(screen, 0, 0);*/
-
-
-	struct Test { char a, c; int b; };
-
-	int a = sizeof(Test);
-
-	std::cout << a;
-
-
+	particles.reserve(100000);
 }
 
+
+int spawnRate = 3;
+int spawnCount = 500;
 
 
 
 void Game::Tick( float /* deltaTime */ )
 {
-
+	screen->Clear(0x000000);
 	
+	// Spawn particles
+	for (int i = 0; i < spawnRate; i++)
+	{
+		particles.push_back(Particle(float2(rand() % SCRWIDTH, 0)));
+	}
+
+	// animate particles
+	for (auto& p : particles)
+	{
+		p.CheckBlocked(screen);
+		p.Move();
+		p.Draw(screen);
+	}
 
 }
+
+
